3c4389b3b8388ade2937f446942d15eb7785ee7c
peffles <wyattpefley@gmail.com>
2018-11-11 11:53:59 -0800

    aws stuff

--- src/__test__/auth-router.test.js
+++ src/__test__/auth-router.test.js
@@ -6,17 +6,17 @@ const { pRemoveAccountMock, pCreateAccountMock } = require('./lib/mock-account')
 
 const API_URL = `http://localhost:${process.env.PORT}`;
 
-describe('Auth Router', () => {
+describe('Auth Router Tests', () => {
   beforeAll(startServer);
   afterAll(stopServer);
   afterEach(pRemoveAccountMock);
 
   describe('POST route /signup', () => {
-    test('should return a 200 status code and a TOKEN', () => {
+    test('Should return a 200 status code and a TOKEN', () => {
       return superagent.post(`${API_URL}/signup`)
         .send({
           username: 'peffles',
-          email: 'wyatt@wyattiscool.com',
+          email: 'peffles@wyattiscool.com',
           password: 'qwerty123',
         })
         .then((response) => {
@@ -24,7 +24,7 @@ describe('Auth Router', () => {
           expect(response.body.token).toBeTruthy();
         });
     });
-    test('should return a 400 status code when no name is sent', () => {
+    test('Should return a 400 status code when no name is sent', () => {
       return superagent.post(`${API_URL}/signup`)
         .send({})
         .then(Promise.reject)
@@ -32,7 +32,7 @@ describe('Auth Router', () => {
           expect(response.status).toEqual(400);
         });
     });
-    test('should return 409 status code due to duplicate name', () => { // eslint-disable-line
+    test('Should return 409 status code due to duplicate name', () => { // eslint-disable-line
       return pCreateAccountMock()
         .then((mock) => {
           const mockAccount = {
@@ -51,7 +51,7 @@ describe('Auth Router', () => {
   });
 
   describe('GET /signin', () => {
-    test('GET /signin should return a 200 status code and TOKEN', () => {
+    test('GET /signin Should return a 200 status code and TOKEN', () => {
       return pCreateAccountMock()
         .then((mock) => {
           return superagent.get(`${API_URL}/signin`)
